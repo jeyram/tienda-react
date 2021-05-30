9b2f94ef5c1a25a83673ea561920141de34d688f
Jorge Ram <jeyramx@gmail.com>
2021-05-30 11:52:03 -0500

    Add Material UI Card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
       <h1>Tienda en Linea V.0.1</h1>
+      <Product/>
     </div>
   );
 }
