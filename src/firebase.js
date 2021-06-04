641507b84fd44654c0a715886ec9189be4198c5e
Jorge Ram <jeyramx@gmail.com>
2021-06-04 04:41:20 -0500

    Security update webform

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,4 @@
 import firebase from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDqFBGdQK1dg5a1xAIM_NIxysL2_NoDjK4",
-  authDomain: "tienda-yza.firebaseapp.com",
-  projectId: "tienda-yza",
-  storageBucket: "tienda-yza.appspot.com",
-  messagingSenderId: "66138749003",
-  appId: "1:66138749003:web:2c4318471512db3a1535ce"
-};
-
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
